c6294a35d08a42bda44021429fd8f7fa7084911f
Pawan Bhatt <pawan.bhatt@ukg.com>
2024-01-22 21:22:47 +0530

    project checkin

---
+++ src/test/java/com/example/springtestapp/SpringTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
